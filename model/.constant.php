189763115da2ae1c41c922714c1f7be8be1e5e9c
johnnyvaca <47849728+johnnyvaca@users.noreply.github.com>
2020-08-27 11:14:34 +0200

    Revert "Revert "model test""
    This reverts commit 699f234d164cb23133d58ee118f7ea803ee6ce75.

---
+++ model/.constant.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbhost = 'web21.swisscenter.com';
+$dbname = 'bargylus_db';
+$user = 'bargylus_admin';
+$pass = 'BargylusPa$$w0rd';
+
+?>
--- model/model.php
+++ model/model.php
@@ -5,3 +5,21 @@
  *USER:marwa
  *DATE:14.05.2020
  */
+
+function getAllItems()
+{
+    require ".constant.php";
+    try {
+        $dbh = new PDO('mysql:host=' . $dbhost . ';dbname=' . $dbname, $user, $pass);
+        $query = 'SELECT * FROM users';
+        $statment = $dbh->prepare($query);//prepare query, il doit faire des vérifications et il va pas exécuter tant
+        //qu'il y a des choses incorrects
+        $statment->execute();//execute query
+        $queryResult = $statment->fetchAll();//prepare result for client cherche tous les résultats
+        var_dump($queryResult);
+        $dbh = null; //refermer une connection quand on a fini
+        return $queryResult;
+    } catch (PDOException $e) {
+        print "Error!: " . $e->getMessage() . "<br/>";
+        return null;
+    }
