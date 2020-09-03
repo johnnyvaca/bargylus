6e9a7e2432ea66ee01775e7e94e0128c9e291d3b
johnnyvaca <47849728+johnnyvaca@users.noreply.github.com>
2020-09-03 22:09:21 +0200

    files

--- model/model.php
+++ model/model.php
@@ -6,20 +6,30 @@
  *DATE:14.05.2020
  */
 
-
-/*
- * connexion à la base de donneés
- */
-function getPDO (){
-    require ".constant.php";
-    $dbh = new PDO('mysql:host=' . $dbhost . ';dbname=' . $dbname, $user, $pass);
-    return $dbh;
+function getUsers()
+{
+    require "model/.constant.php";
+    try {
+        $dbh = new PDO('mysql:host=' . $dbhost . ';dbname=' . $dbname, $user, $pass);
+        $query = 'SELECT * FROM users ';
+        $statment = $dbh->prepare($query);//prepare query, il doit faire des vérifications et il va pas exécuter tant
+        //qu'il y a des choses incorrects
+        $statment->execute();//execute query
+        $queryResult = $statment->fetchAll(PDO::FETCH_ASSOC);//prepare result for client cherche tous les résultats
+        $dbh = null; //refermer une connection quand on a fini
+        if($debug) var_dump($queryResult);
+        return $queryResult;
+    } catch (PDOException $e) {
+        print "Error!: " . $e->getMessage() . "<br/>";
+        return null;
+    }
 }
 
 function getUserByEmail($email)
 {
+    require "model/.constant.php";
     try {
-        $dbh = getPDO();
+        $dbh = new PDO('mysql:host=' . $dbhost . ';dbname=' . $dbname, $user, $pass);
         $query = 'SELECT * FROM users WHERE email=:email';
         $statment = $dbh->prepare($query);//prepare query, il doit faire des vérifications et il va pas exécuter tant
         //qu'il y a des choses incorrects
@@ -35,46 +45,23 @@ function getUserByEmail($email)
 }
 function createUser($user)
 {
-    try {
-        $pdo = getPDO();
-        $query = "INSERT INTO users(email, lastname, firstname, phone_number, registration_date, birth_date, street_home, zip, city, canton, password) 
-                  VALUES (:email, :lastname, :firstname, :phone_number, :registration_date, :birth_date, :street_home, :zip, :city, :canton, :password)";
-        $stmt = $pdo->prepare("$query");
-        $stmt->execute($user);
-        $queryResult = $stmt->fetch(PDO::FETCH_ASSOC);
-        $user['id'] = $pdo->lastInsertId();
 
-        $pdo = null;
-        return $user['id'];
+
+
+    require "model/.constant.php";
+    try {
+        $dbh = new PDO('mysql:host=' . $dbhost . ';dbname=' . $dbname, $user, $pass);
+        $query = "INSERT INTO users( email,  lastname,  firstname,  phone_number,  registration_date,  birth_date,  street_home,  zip,  city,  canton,  password) 
+                  VALUES  ('altin.zili@cpnv.ch','altin','zili',763317057,'2020-09-01','Chemin de Renens 13',1004,'Lausanne','14c4b06b824ec593239362517f538b29')";
+        $stmt = $dbh->prepare($query);
+        $stmt->execute();
+      //  $queryResult = $stmt->fetch(PDO::FETCH_ASSOC);
+        $user['id'] = $dbh->lastInsertId();
+        $dbh = null;
     } catch (PDOException $e) {
         print "Error!:" . $e->getMessage() . "<br/>";
         die();
     }
+    //
 
 }
-
-/*
-author : Marwan
-
-*/
-
-function getWines(){
-
-    require "model/.constant.php";
-
-
-    try {
-        $dbh = getPDO();
-        $query = 'SELECT * FROM bargylus_db.wines';
-        $statment = $dbh->prepare($query);
-        $statment->execute();//prepare query
-        $queryResult = $statment->fetchAll(PDO::FETCH_ASSOC);//prepare result for client
-        $dbh = null;
-        return $queryResult;
-    } catch (PDOException $e) {
-        print "Error!: " . $e->getMessage() . "<br/>";
-        return null;
-    }
-}
-
-
