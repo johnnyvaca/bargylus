ed8ce98e237ddee647addf0b2187d19089e18df0
alhelo-93 <47849816+alhelo-93@users.noreply.github.com>
2020-09-09 03:09:27 +0200

    Update cartdetail.php
    add table to content

--- view/cartdetail.php
+++ view/cartdetail.php
@@ -15,7 +15,7 @@ $title = "Wine_Details";
 
 <div class="site-section bg-primary py-5 page-title-wrap mt-5">
     <div class="container">
-        <h1><?= $wine ['name'] ," ", $wine['year'] ," ", $wine['type'] ?> </h1>
+        <h1><?= $wine ['name'] ," ", $wine['year'] ," ", $wine['color']; ?> </h1>
     </div>
 </div>
 
@@ -30,10 +30,39 @@ $title = "Wine_Details";
                 </div>
             </div>
             <div class="col-lg-5 ml-auto">
-                <h2 class="text-primary">Wine Details</h2>
+                <h2 class="text-primary"><?= $wine['name']," ", $wine['year'] ?> Details</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non eos inventore aspernatur voluptatibus
                     ratione odit molestias molestiae, illum et impedit veniam modi sunt quas nam mollitia earum
                     perferendis, dolorem. Magni.</p>
+                <table class="table table-bordered">
+                    <thead>
+                    <tr>
+                        <th class="product-thumbnail">Année</th>
+                        <th class="product-name">alcool </th>
+                        <th class="product-price"> Price</th>
+                        <th class="product-quantity">disponibilté</th>
+                        <th class="product-total">Taille</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td class="product-thumbnail">
+                            <?= $wine['year'] ?>
+                        </td>
+                        <td class="product-name">
+                            <?= $wine['alcohol'],"%" ?>
+                        </td>
+                        <td><?= $wine['basic_price'],"CHF"?></td>
+                        <td>
+                            <?= $wine['stock']?>
+                        </td>
+                        <td><?= $wine['size']?></td>
+
+                    </tr>
+
+
+                    </tbody>
+                </table>
 
                 <div class="mb-5">
                     <div class="input-group mb-3" style="max-width: 200px;">
