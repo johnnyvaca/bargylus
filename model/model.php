57dd6e2ef1176a383fa14a5c2beb3d315eacc593
alhelo-93 <47849816+alhelo-93@users.noreply.github.com>
2020-09-15 21:37:26 +0200

    Update model.php
    add detwinesolds

--- model/model.php
+++ model/model.php
@@ -158,10 +158,32 @@ function withdrawWineBottle($id)
         $statment->execute(['id' => $id]);//prepare query
         $queryResult = $statment->fetch(PDO::FETCH_ASSOC);//prepare result for client
         $dbh = null;
-        return $queryResult;
         if ($debug) var_dump($queryResult);
+        return $queryResult;
     } catch (PDOException $e) {
         print "Error!: " . $e->getMessage() . "<br/>";
         return null;
     }
+}
+
+
+function getSolds()
+{
+    // pour l'affichage debuge
+    require "model/.constant.php";
+    try {
+        $dbh = getPDO();
+        $query = 'SELECT * FROM wines INNER JOIN discounts on  wines.discounts_id = discounts.id ';
+        $statment = $dbh->prepare($query);
+        $statment->execute();//prepare query
+        $queryResult = $statment->fetchall(PDO::FETCH_ASSOC);//prepare result for client
+        $dbh = null;
+        if ($debug) var_dump($queryResult);
+        return $queryResult;
+        } catch (PDOException $e) {
+        print "Error!: " . $e->getMessage() . "<br/>";
+        return null;
+        }
+
+
 }
