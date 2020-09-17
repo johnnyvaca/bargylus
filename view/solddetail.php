152dc64794704b285ec1ff32674d778e5a626b2c
alhelo-93 <47849816+alhelo-93@users.noreply.github.com>
2020-09-17 08:52:24 +0200

    Create solddetail.php
    add soldDetail

---
+++ view/solddetail.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * *File :solddetail.php
+ *User :Marwan.ALHELO
+ *Project :bargylus
+ *Date:17.09.2020
+ */
+ob_start();
+$title = "Wine_Details";
+?>
+
+
+
+
+
+
+
+
+
+<?php
+$content = ob_get_clean();
+require "gabarit.php";
+?>
+
