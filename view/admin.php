67c12a2d107beb02c060a461425912ea65cc753f
VACA-JARAMILLO Johnny <Johnny.VACA-JARAMILLO@cpnv.ch>
2020-09-28 12:13:22 +0200

    Update admin.php

--- view/admin.php
+++ view/admin.php
@@ -5,35 +5,29 @@ $title = "Bargylus - Admin";
 
 ?>
 <br><br><br><p>sdf</p>
-<table>
-   <thead>
-    <tr>
-    <th>nom vin</th><th>quantité</th><th>prix 1 vin</th><th>num commande</th><th>état</th><th>prix total</th>
-    </tr>
-    </thead>
+<table class="table table-striped">
     <tbody>
     <?php foreach ($orders as $i => $order) {
 if ($i == 0){
-    echo "<tr><td>".$order['id']."</td><td>".$order['firstname']."</td><td  colspan='5'>".$order['lastname']."</td></tr>";
+    echo "<tr class=\"table-danger\"><td>".$order['id']."</td><td>".$order['firstname']."</td><td colspan='3'>".$order['lastname']."</td></tr>";
 
         }else if( $orders[$i]['id'] != $orders[$i-1]['id'] && $i != 0){
-    echo "<tr><td>".$order['id']."</td><td>".$order['firstname']."</td><td  colspan='5'>".$order['lastname']."</td></tr>";
+    echo "<tr class=\"table-danger\"><td>".$order['id']."</td><td>".$order['firstname']."</td><td colspan='3'>".$order['lastname']."</td></tr>";
         }
 
         if ($i == 0){
-        echo "<tr> <td></td><td colspan='6'>".$order['number']."</td></tr>";
+            echo "<tr class=\"table-primary\"> <td></td><td>commande : ".$order['number']." </td><td>total : ".$order['total_price']."</td><td colspan='2'>état : ".$order['state']."</td></tr>";
 
-                                                            }else if( $orders[$i]['number'] != $orders[$i-1]['number'] && $i != 0){
-                                                            echo "<tr> <td></td><td colspan='6'>".$order['number']."</td></tr>";
-                                                                                                                }
+        }else if( $orders[$i]['number'] != $orders[$i-1]['number'] && $i != 0){
+            echo "<tr class=\"table-primary\"> <td></td><td>commande : ".$order['number']." </td><td>total : ".$order['total_price']."</td><td colspan='2'>état : ".$order['state']."</td></tr>";                                          }
         ?>
         <tr>
            <td></td>
+            <td></td>
             <td>nom du vin : <?=$order['winename']?></td>
             <td>quantité : <?=$order['quantity']?></td>
             <td>prix 1 : <?=$order['price_wine']?></td>
-            <td>état : <?=$order['state']?></td>
-            <td>total : <?=$order['total_price']?></td>
+
         </tr>
     <?php
     } ?>
