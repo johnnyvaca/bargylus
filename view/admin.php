8f944cbcfce2077878bd1aee1675fb93400c9113
VACA-JARAMILLO Johnny <Johnny.VACA-JARAMILLO@cpnv.ch>
2020-09-28 10:51:08 +0200

    Update admin.php

--- view/admin.php
+++ view/admin.php
@@ -4,33 +4,36 @@ $title = "Bargylus - Admin";
 
 
 ?>
-<br><br><p>
-    asdas
-</p>
+<br><br><br><p>sdf</p>
 <table>
-    <thead>
+   <thead>
     <tr>
-        <th>id</th><th>prénom</th><th>nom</th><th>nom vin</th><th>quantité</th><th>prix 1 vin</th><th>num commande</th><th>état</th><th>prix total</th>
+    <th>nom vin</th><th>quantité</th><th>prix 1 vin</th><th>num commande</th><th>état</th><th>prix total</th>
     </tr>
     </thead>
     <tbody>
     <?php foreach ($orders as $i => $order) {
-?>
+if ($i == 0){
+    echo "<tr><td>".$order['id']."</td><td>".$order['firstname']."</td><td  colspan='5'>".$order['lastname']."</td></tr>";
 
+        }else if( $orders[$i]['id'] != $orders[$i-1]['id'] && $i != 0){
+    echo "<tr><td>".$order['id']."</td><td>".$order['firstname']."</td><td  colspan='5'>".$order['lastname']."</td></tr>";
+        }
 
+        if ($i == 0){
+        echo "<tr> <td></td><td colspan='6'>".$order['number']."</td></tr>";
+
+                                                            }else if( $orders[$i]['number'] != $orders[$i-1]['number'] && $i != 0){
+                                                            echo "<tr> <td></td><td colspan='6'>".$order['number']."</td></tr>";
+                                                                                                                }
+        ?>
         <tr>
-            <?php if ($i == 0){ ?>
-                <td><?=$i?></td>
-            <?php}else{?>
-        <td>vide</td>
-        <?php}?>
-
-            <td><?=$order['winename']?></td>
-            <td><?=$order['quantity']?></td>
-            <td><?=$order['price_wine']?></td>
-            <td><?=$order['number']?></td>
-            <td><?=$order['state']?></td>
-            <td><?=$order['total_price']?></td>
+           <td></td>
+            <td>nom du vin : <?=$order['winename']?></td>
+            <td>quantité : <?=$order['quantity']?></td>
+            <td>prix 1 : <?=$order['price_wine']?></td>
+            <td>état : <?=$order['state']?></td>
+            <td>total : <?=$order['total_price']?></td>
         </tr>
     <?php
     } ?>
