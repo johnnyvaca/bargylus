1fae373b9cadce9918ae0fe1490c7031dd2f3677
johnnyvaca <47849728+johnnyvaca@users.noreply.github.com>
2020-11-12 19:33:33 +0100

    doc

--- mvc/model/Test.php
+++ mvc/model/Test.php
@@ -1,24 +1,24 @@
 <?php
 
-
+require "model/Dbh.php";
 class Test extends Dbh
 {
-
+    /*
     public function getUsers(){
         $sql = "SELECT * FROM categories";
         $stmt = $this->connect()->query($sql);
     }
+    */
     public function read()
     {
-        $dbh = $this->connect();
+
         try {
+            $pdo = $this->connect();
             $query = "SELECT * FROM categories";
-            $statment = $dbh->query($query);
-            //qu'il y a des choses incorrects
+            $statment = $pdo->prepare($query);
             $statment->execute();//execute query
             $queryResult = $statment->fetchAll(PDO::FETCH_ASSOC);//prepare result for client cherche tous les résultats
-
-            $dbh = null; //refermer une connection quand on a fini
+            //$pdo = null; //refermer une connection quand on a fini
             if ($debug) var_dump($queryResult);
             return $queryResult;
         } catch (PDOException $e) {
