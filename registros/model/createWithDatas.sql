26bd641a578876209ef73ca22576f52f83507857
yogo <47849728+johnnyvaca@users.noreply.github.com>
2022-03-16 15:27:28 +0100

    Update createWithDatas.sql

--- registros/model/createWithDatas.sql
+++ registros/model/createWithDatas.sql
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS `culte` (
   `adultos` varchar(45) DEFAULT NULL,
   `ninos` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `unique` (`users_id`,`culte_id`,`services_id`),
+  UNIQUE KEY `unique` (`date`,`adultos`,`ninos`)
 ) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb3;
 
 -- Listage des données de la table registros.culte : ~0 rows (environ)
@@ -81,7 +81,7 @@ CREATE TABLE IF NOT EXISTS `users_has_services` (
   `culte_id` int NOT NULL,
   `id` int NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `unique` (`users_id`,`culte_id`,`services_id`),
+  UNIQUE KEY `unique` (`users_id`,`culte_id`),
   KEY `fk_users_has_services_services1_idx` (`services_id`),
   KEY `fk_users_has_services_users_idx` (`users_id`),
   KEY `fk_users_has_services_culte1_idx` (`culte_id`),
