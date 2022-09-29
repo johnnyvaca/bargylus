66e4c76f413bb5531be7b9808a4766796ef812dd
yogo <47849728+johnnyvaca@users.noreply.github.com>
2022-09-29 21:51:41 +0200

    gabarit WIP FAUX

--- view/gabarit.php
+++ view/gabarit.php
@@ -14,25 +14,6 @@
     <title><?= $title; ?> </title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
-
-    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500&display=swap" rel="stylesheet">
-
-    <script type="text/javascript" src="../js/jquery-3.3.1.min.js"></script>
-    <script type="text/javascript" src="../js/bootstrap.min.js"></script>
-
-    <link rel="stylesheet" href="fonts/icomoon/style.css">
-
-    <link href='fullcalendar/packages/core/main.css' rel='stylesheet'/>
-    <link href='fullcalendar/packages/daygrid/main.css' rel='stylesheet'/>
-
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="../css/bootstrap/bootstrap.css">
-
-    <!-- Style -->
-    <link rel="stylesheet" href="../css/style.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 
     <link href="https://fonts.googleapis.com/css?family=Cinzel:400,700|Montserrat:400,700|Roboto&display=swap"
@@ -40,6 +21,7 @@
 
     <link rel="stylesheet" href="/fonts/icomoon/style.css">
 
+    <link rel="stylesheet" href="/css/bootstrap/bootstrap.css">
     <link rel="stylesheet" href="/css/jquery-ui.css">
     <link rel="stylesheet" href="/css/owl.carousel.min.css">
     <link rel="stylesheet" href="/css/owl.theme.default.min.css">
@@ -49,9 +31,6 @@
 
     <link rel="stylesheet" href="/css/bootstrap-datepicker.css">
 
-
-    <script src="/js/stayle.js"></script>
-
     <link rel="stylesheet" href="/fonts/flaticon/font/flaticon.css">
 
     <link rel="stylesheet" href="/css/aos.css">
@@ -60,129 +39,158 @@
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/main.css">
 
-    <style>
-        .scrollit {
-            overflow: scroll;
-            height: 10%;
-        }
-    </style>
+
 </head>
 
-<body data-spy="scroll" data-target=".site-navbar-target" data-offset="200" style="background:  transparent">
-<div class="container-fluid pt-0 pb-0 mb-0 pb-0 py-0">
-    <div class="row   bg-info mb-0 mt-0">
-        <nav class="navbar  navbar-expand-lg navbar-light bg-light  pt-0 mt-0 pb-0 mb-0 pb-0">
-
-            <a class="navbar-brand" href="#"></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse  navbar-collapse pt-0 pb-0 mb-0" id="navbarSupportedContent">
-                <ul class="navbar-nav  align-self-center align-items-center  mr-auto mb-0 pb-0">
-                    <li class="nav-item active"><a href="index.php?action=home"
-                                                   class="nav-link text-left text-danger">Quienes somos</a>
-                    </li>
-                    <li class="nav-item"><a href="index.php?action=displaywines"
-                                            class="nav-link text-left text-danger">Administracion</a>
-                    </li>
-                    <li class="nav-item"><a href="index.php?action=about"
-                                            class="nav-link text-left text-danger">Cultos</a>
-                    </li>
-                    <li class="nav-item"><a href="index.php?action=about"
-                                            class="nav-link text-left text-danger pb-0 mb-0"><img
-                                    src="../images/logo.jpg" height="120px" width="160px"></a>
-                    </li>
+<body data-spy="scroll" data-target=".site-navbar-target" data-offset="200" style="background:  yellow">
+<div class="site-wrap">
 
 
-                    <li class="nav-item"><a href="index.php?action=contact"
-                                            class="nav-link text-left text-danger">Eventos</a>
-                    </li>
-                    <?php if (isset($_SESSION["user"])) { ?>
-                        <li class="nav-item btn-danger"><?= '<a href="index.php?action=logout"
-                                                               class="nav-link text-center white">Déconnexion</a>' ?></li>
-                        <li class="nav-item btn btn-danger"><a
-                                    href="index.php?action=profil&id=<?= $_SESSION['user']['id'] ?>"
-                                    class="nav-link text-center white">Profil</a></li>
-
-                        <?php if ($_SESSION["user"]["droits"] == 1) { ?>
-                            <li class="nav-item btn btn-danger"><?= '<a href="index.php?action=adminPage"
-                                                               class="nav-link text-center white">Commandes</a>' ?></li>
-                            <li class="nav-item btn btn-danger"><a href="index.php?action=listOfAccounts"
-                                                                   class="nav-link text-center white">Comptes</a>
-                            </li>
-                        <?php } ?>
-                    <?php } else { ?>
-
-                        <!--  <li class="btn btn-danger"><?= '<a href="index.php?action=login"
-                                                               class="nav-link text-center white">Donaciones</a>' ?></li> -->
-
-                        <li class="nav-item border border-danger pb-0 mb-0 py-0 h-100"><a href="index.php?action=donations"
-                                                class="nav-link text-left text-danger ">Donaciones</a>
-                        </li>
-                        <li class="nav-item dropdown border border-danger h-100">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                               data-toggle="dropdown">
-                                Idioma
-                            </a>
-                            <div class="dropdown-menu mb-0 pb-0" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="#">Francais</a>
-                                <a class="dropdown-item" href="#">Deutsch</a>
-                                <a class="dropdown-item" href="#">Español</a>
-                                <a class="dropdown-item" href="#">Italiano</a>
-                            </div>
-                        </li>
-
-
-                    <?php } ?>
-
-
-                </ul>
+    <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+            <div class="site-mobile-menu-close mt-3">
+                <span class="icon-close2 js-menu-toggle"></span>
             </div>
-        </nav>
+        </div>
+        <div class="site-mobile-menu-body"></div>
     </div>
 
-</div>
-<!--<div class="splash">
-    <img src="../images/splash.gif" width="100%">
-</div> -->
 
+    <div class="header-top">
+        <!--   <div class="container">
+
+                  <div class="row align-items-start">  <div class="col-12 text-center">
+                        <a href="index.php?action=home" class="site-logo">
+                             <img src="/images/logo.png" alt="Image" class="img-fluid " id="logo">
+                       </a>
+
+                       </div>
+                   </div>
+
+
+          </div>-->
+        <?= getFlashMessage() ?>
+
+        <div class="site-navbar py-2 js-sticky-header site-navbar-target d-none pl-0 d-lg-block" role="banner">
+            <div class="container ">
+                <div class="d-flex align-items-center">
+
+                    <div class="mx-auto">
+                        <a href="#" class="mx-auto d-inline-block d-lg-none site-menu-toggle js-menu-toggle text-black"><span
+                                    class="icon-menu h3"></span></a>
+                        <nav class="site-navigation position-relative text-left" role="navigation">
+                            <ul class="site-menu main-menu js-clone-nav mx-auto d-none pl-0 d-lg-block border-none">
+                                <li class="active"><a href="index.php?action=home"
+                                                      class="nav-link text-left text-danger">Quienes somos</a>
+                                </li>
+                                <li class="active"><a href="index.php?action=displaywines"
+                                                class="nav-link text-left text-danger">Administracion</a>
+                                </li>
+                                <li class="active"><a href="index.php?action=about"
+                                                        class="nav-link text-left text-danger">Cultos</a>
+                                </li>
+                                <li class="active"><a href="index.php?action=about"
+                                                        class="nav-link text-left text-danger pb-0 mb-0 pt-0 mt-0"><img
+                                                src="../images/logo.jpg" height="100px" width="230px"></a>
+                                </li>
+
+
+                                <li class="nav-item"><a href="index.php?action=contact"
+                                                        class="nav-link text-left text-danger">Eventos</a>
+                                </li>
+                                <?php if (isset($_SESSION["user"])) { ?>
+                                    <li class="nav-item btn-danger"><?= '<a href="index.php?action=logout"
+                                                               class="nav-link text-center white">Déconnexion</a>' ?></li>
+                                    <li class="nav-item btn btn-danger"><a
+                                                href="index.php?action=profil&id=<?= $_SESSION['user']['id'] ?>"
+                                                class="nav-link text-center white">Profil</a></li>
+
+                                    <?php if ($_SESSION["user"]["droits"] == 1) { ?>
+                                        <li class="nav-item btn btn-danger"><?= '<a href="index.php?action=adminPage"
+                                                               class="nav-link text-center white">Commandes</a>' ?></li>
+                                        <li class="nav-item btn btn-danger"><a href="index.php?action=listOfAccounts"
+                                                                               class="nav-link text-center white">Comptes</a>
+                                        </li>
+                                    <?php } ?>
+                                <?php } else { ?>
+
+                                    <!--  <li class="btn btn-danger"><?= '<a href="index.php?action=login"
+                                                               class="nav-link text-center white">Donaciones</a>' ?></li> -->
+
+                                    <li class="nav-item border border-danger pb-0 mb-0 py-0 h-100"><a
+                                                href="index.php?action=donations"
+                                                class="nav-link text-left text-danger ">Donaciones</a>
+                                    </li>
+                                    <li class="nav-item dropdown border border-danger h-100">
+                                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                           data-toggle="dropdown">
+                                            Idioma
+                                        </a>
+                                        <div class="dropdown-menu mb-0 pb-0" aria-labelledby="navbarDropdown">
+                                            <a class="dropdown-item" href="#">Francais</a>
+                                            <a class="dropdown-item" href="#">Deutsch</a>
+                                            <a class="dropdown-item" href="#">Español</a>
+                                            <a class="dropdown-item" href="#">Italiano</a>
+                                        </div>
+                                    </li>
+
+
+                                <?php } ?>
+
+
+                            </ul>
+                        </nav>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div><hr class=" pt-0 pb-0 mb-0 pb-0"
+              style="height:2px;border-width:0;color:#ff0000;background-color:#ff0000; margin-top: 0;">
+</div>
+
+<!--
+<div class="splash">
+    <img src="../images/splash.gif" width="100%">
+</div>
+-->
 
 <!-- J'ajoute un flashmessage pour la connexion) -->
 
 
-<?= getFlashMessage() ?>
-<hr class=" pt-0 pb-0 mb-0 pb-0" style="height:2px;border-width:0;color:#ff0000;background-color:#ff0000; margin-top: 0;">
+
 
 
 <?= $content; ?>
 
+<div class="footer">
+    <div class="container">
+        <div class="row bg-danger">
+            <div class="col-3 social-icons  ">
+                <a href="#"><span class="icon-facebook"></span></a>
+                <a href="#"><span class="icon-twitter"></span></a>
+                <a href="#"><span class="icon-youtube"></span></a>
+                <a href="#"><span class="icon-instagram"></span></a>
+            </div>
 
-<div class="bg-danger text-lg-start text-muted border-bottom h-100 pt-2  "
-     style="display: flex;justify-content: space-between;">
-    <div  style="width: 400px;padding-top: 40px;padding-right: 0;padding-left: 0"
-         class=" bg-danger social-icons align-items-center">
-        <a href="#"><span class="icon-facebook"></span></a>
-        <a href="#"><span class="icon-twitter"></span></a>
-        <a href="#"><span class="icon-youtube"></span></a>
-        <a href="#"><span class="icon-instagram"></span></a>
+            <div class="col-3 text text-light">
+                <ul style="list-style-type: none;margin: 0;padding: 0;position: absolute" class="">
+                    <li><span style="font-weight: bold">Français</span>
+                        <span>- Jean Poul     </span><span>     079 232 40 06</span>
+                    </li>
+                    <li><span style="font-weight: bold">Deutsch</span>
+                        <span>- Carlos Schmidt</span><span>     079 232 40 06</span></li>
+                    <li><span style="font-weight: bold">Español</span>
+                        <span>- Armando Valdez</span><span>     079 232 40 06</span></li>
+                    <li><span style="font-weight: bold">Italiano</span>
+                        <span>- Fabio Ricardi </span><span>     079 232 40 06</span>
+                    </li>
+                </ul>
+            </div>
+            <!--  <div style="background: green; height: 50px;width: 50px; align-items: center;justify-content: center"></div>-->
+        </div>
     </div>
-
-    <div class=" bg-danger text text-light" style="width: 400px;height: 300px right: 0;top: 0">
-        <ul style="list-style-type: none;margin: 0;padding: 0;position: absolute" class="numbers2">
-            <li><span style="font-weight: bold">Français</span>
-                <span>- Jean Poul     </span><span>     079 232 40 06</span>
-            </li>
-            <li><span style="font-weight: bold">Deutsch</span>
-                <span>- Carlos Schmidt</span><span>     079 232 40 06</span></li>
-            <li><span style="font-weight: bold">Español</span>
-                <span>- Armando Valdez</span><span>     079 232 40 06</span></li>
-            <li><span style="font-weight: bold">Italiano</span>
-                <span>- Fabio Ricardi </span><span>     079 232 40 06</span>
-            </li>
-        </ul>
-    </div>
-    <!--  <div style="background: green; height: 50px;width: 50px; align-items: center;justify-content: center"></div>-->
 </div>
 <!-- <div style="background: #c527b8">
      <p>
@@ -210,7 +218,6 @@
                 stroke="#ff5e15"/>
     </svg>
 </div>
-</body>
 
 <script src="/js/jquery-3.3.1.min.js"></script>
 <script src="/js/jquery-migrate-3.0.1.min.js"></script>
@@ -232,87 +239,7 @@
 <script src="/js/gabarit.js"></script>
 
 
-<script src="../js/jquery-3.3.1.min.js"></script>
-<script src="../js/popper.min.js"></script>
-<script src="../js/bootstrap.min.js"></script>
-
-<script src='../fullcalendar/packages/core/main.js'></script>
-<script src='../fullcalendar/packages/interaction/main.js'></script>
-<script src='../fullcalendar/packages/daygrid/main.js'></script>
-
-<script>
-    document.addEventListener('DOMContentLoaded', function () {
-        var calendarEl = document.getElementById('calendar');
-
-        var calendar = new FullCalendar.Calendar(calendarEl, {
-            plugins: ['interaction', 'dayGrid'],
-            defaultDate: '2020-02-12',
-            editable: true,
-            eventLimit: true, // allow "more" link when too many events
-            events: [
-                {
-                    title: 'All Day Event',
-                    start: '2020-02-01'
-                },
-                {
-                    title: 'Long Event',
-                    start: '2020-02-07',
-                    end: '2020-02-10'
-                },
-                {
-                    groupId: 999,
-                    title: 'Repeating Event',
-                    start: '2020-02-09T16:00:00'
-                },
-                {
-                    groupId: 999,
-                    title: 'Repeating Event',
-                    start: '2020-02-16T16:00:00'
-                },
-                {
-                    title: 'Conference',
-                    start: '2020-02-11',
-                    end: '2020-02-13'
-                },
-                {
-                    title: 'Meeting',
-                    start: '2020-02-12T10:30:00',
-                    end: '2020-02-12T12:30:00'
-                },
-                {
-                    title: 'Lunch',
-                    start: '2020-02-12T12:00:00'
-                },
-                {
-                    title: 'Meeting',
-                    start: '2020-02-12T14:30:00'
-                },
-                {
-                    title: 'Happy Hour',
-                    start: '2020-02-12T17:30:00'
-                },
-                {
-                    title: 'Dinner',
-                    start: '2020-02-12T20:00:00'
-                },
-                {
-                    title: 'Birthday Party',
-                    start: '2020-02-13T07:00:00'
-                },
-                {
-                    title: 'Click for Google',
-                    url: 'http://google.com/',
-                    start: '2020-02-28'
-                }
-            ]
-        });
-
-        calendar.render();
-    });
-
-</script>
-
-<script src="js/main.js"></script>
+<script src="/js/signup.js"></script>
 </body>
 
 </html>
