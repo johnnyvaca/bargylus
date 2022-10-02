53128fee898283219b5de0efc5a1ebabd67b0ce1
yogo <47849728+johnnyvaca@users.noreply.github.com>
2022-10-03 01:08:49 +0200

    wip

--- view/wines.php
+++ view/wines.php
@@ -34,48 +34,49 @@ $title = "Bargylus- wines";
                 <div class="btn btn-primary btn-sm "><a href="index.php?action=addwinePage"
                                                         class="nav-link text-center white"> Ajouter un vin </a></div>
             <?php } ?>
-            <div class="row">
-                <?php foreach ($wines
+            <div class="row  bg-info">
+                <?php foreach ($wines as $i => $wine) { ?>
+                    <div class="col-lg-4 mb-5 col-md-6 bg-success justify-content-center w-100">
 
-                as $i => $wine) { ?>
-                <div class="col-lg-4 mb-5 col-md-6">
+                        <div class="wine_v_1 text-center bg-dark justify-content-center" style="">
 
-                    <div class="wine_v_1 text-center justify-content-center bg-light">
+                            <a href="index.php?action=winedetail&id=<?= $wine['id'] ?>"
+                               class="thumbnail d-block mb-4 position-relative"><img
+                                        src="images/<?= $wine['photo'] ?>" alt="cliquer ici" class="img-fluid"></a>
+                            <div class="position-absolute bg-danger w-100 justify-content-center" style="margin-top: -25px">
+                                <h3 class="heading mb-1 justify-content-center bg-warning w-75"><a
+                                            href="#"><?= $wine['winename'], " ", $wine['color'], " ", $wine['year'] ?></a>
+                                </h3>
+                            </div>
+                          <!--  <div class="wine-actions">
+                                <h3 class="heading-2"><a
+                                            href="#"><?= $wine['winename'], " ", $wine['color'], " ", $wine['year'] ?></a>
+                                </h3>
+                                <?php if ($wine['discounts_id'] != 1) { ?>
+                                    <span class="price d-block"><del> <?= $wine['basic_price'] ?> CHF </del></span>
+                                <?php } ?>
+                                <span class="price d-block"> <?= $soldWineprice[$i] ?>CHF</span>
 
-                        <a href="index.php?action=winedetail&id=<?= $wine['id'] ?>"
-                           class="thumbnail d-block" style="z-index: 2"><img
-                                    src="images/<?= $wine['photo'] ?>" alt="cliquer ici" class="img-fluid"></a>
-                        si le vin est épuisé
-                        <?php if ($wine['stock'] > 0) { ?>
-                            <a href="index.php?action=winesBasket&id=<?= $wine['id'] ?>" class="btn add"><span
-                                        class="icon-shopping-bag mr-3"></span> ajouter au panier </a>
-                        <?php } else { ?>
-                            <span>vin épuisé</span>
-                        <?php } ?>
 
+                                <div class="rating">
+                                    <span class="icon-star"></span>
+                                    <span class="icon-star"></span>
+                                    <span class="icon-star"></span>
+                                    <span class="icon-star"></span>
+                                    <span class="icon-star-o"></span>
+                                </div>
+
+                                <?php if ($wine['stock'] > 0) { ?>
+                                    <a href="index.php?action=winesBasket&id=<?= $wine['id'] ?>" class="btn add"><span
+                                                class="icon-shopping-bag mr-3"></span> ajouter au panier </a>
+                                <?php } else { ?>
+                                    <span>vin épuisé</span>
+                                <?php } ?>
+                            </div> -->
+                        </div>
                     </div>
-                    <div class=" pt-5 ml-5 mr-5 bg-success justify-content-center " style="margin-top: -100px">
-                        <h3 class="heading ">
-                            <a href="#"><?= $wine['winename'], " ", $wine['color'], " ", $wine['year'] ?></a>
-                        </h3>
-                    </div>
-
-                </div>
-                <?php if ($_SESSION["user"]["droits"] == 1) { ?>
-                    <nav class="site-navigation position-relative text-left" role="navigation">
-                        <ul class="site-menu main-menu js-clone-nav mx-auto d-none pl-0 d-lg-block border-none">
-                            <li class="btn btn-primary btn-sm"><a
-                                        href="index.php?action=editywine&id=<?= $wine['id'] ?>"
-                                        class="nav-link text-center white">Modifier</a></li>
-
-                            <li class="btn btn-primary btn-sm"><a
-                                        href="index.php?action=deletewine&id=<?= $wine['id'] ?>"
-                                        class="nav-link text-center white">Supprimer</a></li>
-                        </ul>
-                    </nav>
                 <?php } ?>
             </div>
-        <?php } ?>
 
         </div>
     </div>
